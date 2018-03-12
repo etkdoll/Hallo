2ed597d4af7146d616f0cb3fe9a638b51b4f707f
etkdoll <lmcdoll@yahoo.com>
2018-03-12 16:35:08 +0000

    changed to dude

--- app.rb
+++ app.rb
@@ -1,11 +1,7 @@
  require 'sinatra'
  class HelloWorld < Sinatra::Base
  get '/' do
-<<<<<<< HEAD
   "Hello, dude!"
-=======
-  "Hello, world!"
->>>>>>> a6c3b094217efd53ecdf1b141ecbf5601b924a41
 end
  get '/:name' do
     "Hello, #{params[:name]}!"
